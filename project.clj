63eb0332a9e21d932a6830a3fef15dfaa4280a00
Bobby Towers <porkostomus@mail.com>
2019-06-23 15:32:00 -0700

    use heroku

--- project.clj
+++ project.clj
@@ -1,17 +1,16 @@
-(defproject just-juxt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+  :description "Demo Clojure web app"
+  :url "http://clojure-getting-started.herokuapp.com"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]
+                 [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.0"]
                  [clj-http "3.10.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler just-juxt.handler/handler}
-  :main just-juxt.handler
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                 [org.clojure/data.json "0.2.6"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojure-getting-started-standalone.jar"
+  :profiles {:production {:env {:production true}}})
