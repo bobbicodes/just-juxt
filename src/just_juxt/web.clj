1e6fb73889360a8bacbfc8332365e728700f9b85
Bobby Towers <porkostomus@mail.com>
2019-06-23 15:39:19 -0700

    update namespace to just-juxt.web

--- src/just_juxt/web.clj
+++ src/just_juxt/web.clj
@@ -1,4 +1,4 @@
-(ns clojure-getting-started.web
+(ns just-juxt.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
