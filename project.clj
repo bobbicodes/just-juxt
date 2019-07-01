51ca95956bfd4618247dc6f928e2981649f5aeaa
Bobby Towers <porkostomus@mail.com>
2019-06-30 17:40:23 -0700

    add dep for twitter-api

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject just-juxt "1.0.0-SNAPSHOT"
   :description "Just-juxt web app"
-  :url "http://just-juxt.herokuapp.com"
+  :url "just-juxt.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -8,7 +8,9 @@
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
                  [clj-http "3.10.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [instaparse "1.4.10"]
+                 [twitter-api "1.8.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
