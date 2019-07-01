66624cff455f63af909ae3e814b10e3512bd9cbf
Bobby Towers <porkostomus@mail.com>
2019-06-30 23:22:51 -0700

    add dep for next.jdbc

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [clj-http "3.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [instaparse "1.4.10"]
-                 [twitter-api "1.8.0"]]
+                 [twitter-api "1.8.0"]
+                 [seancorfield/next.jdbc "1.0.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
